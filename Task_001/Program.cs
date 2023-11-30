535f140a4e20e071d3e8b8f4c5fec8db06266f68
Vladimir Solovev <versnabo2016@yandex.ru>
2023-11-30 17:53:04 +0300

    Initial commit

---
+++ Task_001/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GenerateArray(int size, int leftRange, int rigthRange)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+
+    for(int i = 0; i < size; i++)
+    {
+        tempArray[i] = rand.Next(leftRange, rigthRange + 1);
+    }
+    return tempArray;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+void PrintAnswer(int[] Array)
+{
+    int Answer = 0;
+    for(int i = 0; i < Array.Length; i++)
+    {
+        if(Array[i] >= 20 && Array[i] <= 90)
+        {
+            Answer++;
+        }
+    }
+    Console.WriteLine(Answer);
+}
+
+int size = ReadInt("Введите размер массива: ");
+int[] myArray = GenerateArray(size, 1, 100);
+PrintArray(myArray);
+PrintAnswer(myArray);
---
+++ Task_003/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+using System.Text.RegularExpressions;
+
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double[] GenerateArray(int size, int leftRange, int rigthRange)
+{
+    double[] tempArray = new double[size];
+    Random rand = new Random();
+
+    for(int i = 0; i < size; i++)
+    {
+        tempArray[i] = Math.Round(rand.Next(leftRange, rigthRange) + rand.NextDouble(), 2);
+    }
+    return tempArray;
+}
+
+void PrintArray(double[] array)
+{
+    Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+void PrintAnswer(double[] Array)
+{
+    double Nmin = 0;
+    double Nmax = 0;
+    for(int i = 0; i < Array.Length; i++)
+    {
+        if(Array[i] > Nmax)
+        {
+            Nmax = Array[i];
+        }        
+    }
+    Nmin = Nmax;
+    for(int i = 0; i < Array.Length; i++)
+    {
+        if(Array[i] < Nmin)
+        {
+            Nmin = Array[i];
+        }
+    }    
+        Console.WriteLine(Nmax - Nmin);
+}
+
+int size = ReadInt("Введите размер массива: ");
+double[] myArray = GenerateArray(size, 1, 100);
+PrintArray(myArray);
+PrintAnswer(myArray);
